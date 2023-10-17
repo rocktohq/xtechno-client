062d8a317708af2eabdd1c47544611bb825b477f
Saidul Mursalin <itzmonir@gmail.com>
2023-10-17 23:17:06 +0600

    Initial Update

---
+++ README.md
@@ -0,0 +1,3 @@
+# xTechno
+
+xTechno
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-center text-primary">xTechno</h1>
+    </div>
+  )
+}
+
+export default App
