1f10989e60b295dec2eed302e0dba7b588c5f835
Saidul Mursalin <itzmonir@gmail.com>
2023-10-20 09:58:44 +0600

    Error Page Updated

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,20 @@
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Link, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
+  const error = useRouteError();
+
   return (
-    <div>
-      Error Page
-    </div>
+    <HelmetProvider>
+      <Helmet>
+        <title>{`${error?.status}`}</title>
+      </Helmet>
+      <div className="h-screen flex flex-col justify-center items-center space-y-2">
+        <h1 className="text-center text-4xl lg:text-5xl font-bold">Error: {error?.status}</h1>
+        <p className="text-xl lg:text-2xl font-medium">{error?.statusText}</p>
+        <Link className="text-center"><button className="btn btn-secondary rounded">&#171; Home Page</button></Link>
+      </div>
+    </HelmetProvider>
   )
 }
 
