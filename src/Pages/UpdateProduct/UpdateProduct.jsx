b221fa68d8387f2d1494b8d1eab989bfeebcd80d
Saidul Mursalin <itzmonir@gmail.com>
2023-10-20 09:40:49 +0600

    Validation Added

--- src/Pages/UpdateProduct/UpdateProduct.jsx
+++ src/Pages/UpdateProduct/UpdateProduct.jsx
@@ -20,15 +20,24 @@ const UpdateProduct = () => {
 
     const form = e.target;
     const name = form.name.value;
-    const brand = form.brand.value;
+    let brand = form.brand.value;
     const type = form.type.value;
     const rating = form.rating.value;
     const image = form.image.value;
     const price = form.price.value;
 
+    // Validations
+    if (name === "" && brand === "" && type === "" && rating === "" && image === "" && price === "") return toast.error("All fields are required");
+    else if (name === "") return toast.error("Product name is required");
+    else if (brand === "") brand = product.brand;
+    else if (type === "") return toast.error("Product type is required");
+    else if (rating === "") return toast.error("Product rating is required");
+    else if (image === "") return toast.error("Product image is required");
+    else if (price === "") return toast.error("Product price is required");
+
     const product = { name, brand, type, rating: parseFloat(rating), image, price };
     // console.log(product)
-    
+
     fetch(`https://xtechno-server.vercel.app/products/${_id}`, {
       method: "PUT",
       headers: {
